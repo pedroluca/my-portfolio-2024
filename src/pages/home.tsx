55ef21718bb9de6ca240f0e0eac02cc87e38ce37
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-07-02 13:33:28 -0300

    feat: replace work and college title on main screen and add new border to bottom of main screen frame

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,11 +7,11 @@ import { ScrollToTopButton } from '../components/scroll-to-top'
 export function Home() {
   return (
     <>
-      <main className='p-4 h-screen w-full flex flex-col items-center justify-center'>
+      <main className='p-4 h-screen w-full flex flex-col items-center justify-center border-b border-zinc-800'>
         <div className='flex flex-col'>
           <h1 className='text-4xl text-white'>Pedro Luca Prates</h1>
-          <h2 className='text-xl'>Full Stack Developer</h2>
-          <h2 className='text-xl text-zinc-500'>Estudante de Análise e Desenvolvimento de Sistemas</h2>
+          <h2 className='text-xl'>Fullstack Developer</h2>
+          <h2 className='text-xl text-zinc-500'>Tecnólogo em Análise e Desenvolvimento de Sistemas</h2>
         </div>
         <Link to="about-section" smooth={true} duration={500} className="mt-8">
           <ChevronDown className="w-10 h-10 text-white animate-bounce" />
