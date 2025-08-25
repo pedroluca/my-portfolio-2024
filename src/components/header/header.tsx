bf0bcef540e0f6acdf9c73f8c4a7e49407ffd1c2
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-08-25 17:55:53 -0300

    refactor: change open and close icons, remove styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { PanelRight, X } from 'lucide-react'
+import { PanelRightClose, PanelRightOpen } from 'lucide-react'
 import { NavLink, useNavigate, useLocation } from 'react-router-dom'
 import { twMerge } from 'tailwind-merge'
 import NewLogo from '../../assets/images/logo.svg'
@@ -31,13 +31,13 @@ export function Header () {
   }
 
   return (
-    <header className='flex px-3 py-1 w-full h-16 items-center fixed justify-between backdrop-blur-md border-b border-white/20 z-10'>
+    <header className='flex px-3 lg:px-[25%] py-1 w-full h-16 items-center fixed justify-between backdrop-blur-md border-b border-white/20 z-10'>
       <NavLink to="/">
         <img src={NewLogo} className='size-10' />
       </NavLink>
       <div className='flex'>
         <button onClick={handleMenu} className='lg:hidden'>
-          <PanelRight />
+          <PanelRightOpen />
         </button>
         <nav id='navbar' className={
           twMerge(
@@ -47,18 +47,18 @@ export function Header () {
         }>
           <div className='py-4 px-5 bg-zinc-950 w-72 flex flex-col border-l lg:border-0 border-white/20 lg:flex-row lg:items-center lg:w-auto lg:p-0 lg:bg-transparent'>
             <button className='self-end lg:hidden' onClick={handleMenu}>
-              <X />
+              <PanelRightClose />
             </button>
             <img src={NewLogo} className='size-10 self-center mb-2 lg:hidden' />
             <hr className='border-white/20 lg:hidden' />
             <ul className='flex flex-col gap-2 px-1 mt-4 text-lg lg:flex-row lg:items-center lg:mt-0'>
               <li>
-                <button onClick={() => handleNavLinkClick('about-section')} className='text-zinc-400 font-semibold hover:text-white lg:hover:bg-white/20 transition px-2 py-1 lg:hover:rounded-md w-full text-left'>
+                <button onClick={() => handleNavLinkClick('about-section')} className='text-zinc-400 font-semibold lg:hover:bg-white/20 transition px-2 py-1 lg:rounded-md w-full text-left'>
                   Sobre
                 </button>
               </li>
               <li>
-                <button onClick={() => handleNavLinkClick('projects-section')} className='text-zinc-400 font-semibold hover:text-white lg:hover:bg-white/20 transition px-2 py-1 lg:hover:rounded-md w-full text-left'>
+                <button onClick={() => handleNavLinkClick('projects-section')} className='text-zinc-400 font-semibold lg:hover:bg-white/20 transition px-2 py-1 lg:rounded-md w-full text-left'>
                   Projetos
                 </button>
               </li>
