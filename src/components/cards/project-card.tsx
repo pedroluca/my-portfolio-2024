ae0d64d438650c356e6be24723c0d89e75439862
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-08-28 16:46:37 -0300

    refactor: change glow color on cards hover

--- src/components/cards/project-card.tsx
+++ src/components/cards/project-card.tsx
@@ -13,15 +13,15 @@ export function ProjectCard(props: ProjectCardProps) {
     <div
       className={`project-card ${props.url ? 'cursor-pointer' : 'cursor-default'} bg-zinc-900 text-white rounded-lg overflow-hidden transition-all duration-300 ease-in-out`}
       style={{ 
-        boxShadow: '0 1px 4px rgba(255, 255, 255, 0.3)',
+        boxShadow: '0 1px 4px rgba(30, 144, 255, 0.3)',
         transition: 'box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out'
       }}
       onMouseEnter={(e) => {
-        e.currentTarget.style.boxShadow = '0 0 8px rgba(255, 255, 255, 0.4), 0 0 16px rgba(255, 255, 255, 0.2)'
+        e.currentTarget.style.boxShadow = '0 0 8px rgba(30, 144, 255, 0.4), 0 0 16px rgba(30, 144, 255, 0.2)'
         e.currentTarget.style.transform = 'scale(1.05)'
       }}
       onMouseLeave={(e) => {
-        e.currentTarget.style.boxShadow = '0 1px 4px rgba(255, 255, 255, 0.3)'
+        e.currentTarget.style.boxShadow = '0 1px 4px rgba(30, 144, 255, 0.3)'
         e.currentTarget.style.transform = 'scale(1)'
       }}
       onClick={handleCardClick}
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,39 +1,39 @@
-import { PageTitle } from "../components/page-title/page-title"
-import { ProjectCard } from "../components/cards/project-card"
-import { MoveRight } from "lucide-react"
+import { PageTitle } from '../components/page-title/page-title'
+import { ProjectCard } from '../components/cards/project-card'
+import { MoveRight } from 'lucide-react'
 
 export function Projects() {
   return (
-    <section className="py-4 px-6 lg:px-[25%] w-full flex flex-col gap-3 text-justify pt-24 mb-16" id="projects-section">
+    <section className='py-4 px-6 lg:px-[25%] w-full flex flex-col gap-3 text-justify pt-24 mb-16' id='projects-section'>
       <PageTitle>Projetos</PageTitle>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
         <ProjectCard
-          title="TrainLog"
-          description="Site para acompanhamento de treinos e exercícios, com temporizador de intervalos incluso e compartilhamento de treinos." 
-          url="https://trainlogapp.vercel.app/"
+          title='TrainLog'
+          description='Site para acompanhamento de treinos e exercícios, com temporizador de intervalos incluso e compartilhamento de treinos.' 
+          url='https://trainlogapp.vercel.app/'
         />
         <ProjectCard
-          title="Site para I Olimpíadas Científicas do Território Sertão Produtivo"
-          description="Site para divulgação de datas, inscrições, e contato dos participantes das I Olimpíadas Científicas do Território Sertão Produtivo." 
-          url="https://www.olimpiadasdosertaoprodutivo.com/"
+          title='I Olimpíadas Científicas do Território Sertão Produtivo'
+          description='Site para divulgação de datas, inscrições, e contato dos participantes das I Olimpíadas Científicas do Território Sertão Produtivo.' 
+          url='https://www.olimpiadasdosertaoprodutivo.com/'
         />
         <ProjectCard
-          title="Blog Criança Alerta" 
-          description="Blog educativo com o intuito de ensinar para as crianças e adolescentes sobre seus direitos." 
-          url="https://www.blogcriancaalerta.epizy.com/" 
+          title='Blog Criança Alerta' 
+          description='Blog educativo com o intuito de ensinar para as crianças e adolescentes sobre seus direitos.' 
+          url='https://www.blogcriancaalerta.epizy.com/' 
         />
         <ProjectCard
-          title="Gerenciador de faturas" 
-          description="Um simples site para gerenciamento de fatores e compras no cartão de crédito." 
-          url="https://billing-manager.vercel.app/" 
+          title='Gerenciador de faturas' 
+          description='Um simples site para gerenciamento de fatores e compras no cartão de crédito.' 
+          url='https://billing-manager.vercel.app/' 
         />
         <ProjectCard
-          title="Repaginação do site do IF Baiano campus Guanambi" 
-          description="Atividade acadêmica cujo intuito era desenvolver ou repaginar um site" 
-          url="https://repage-if-baiano.vercel.app/" 
+          title='Site do IF Baiano campus Guanambi' 
+          description='Atividade acadêmica cujo intuito era desenvolver ou repaginar um site' 
+          url='https://repage-if-baiano.vercel.app/' 
         />
       </div>
-      <p className="mt-6 text-gray-400 flex gap-2 items-center hover:cursor-pointer hover:text-white transition-colors" onClick={() => window.open("https://github.com/pedroluca", "_blank")}>
+      <p className='mt-6 text-gray-400 flex gap-2 items-center hover:cursor-pointer hover:text-white transition-colors' onClick={() => window.open('https://github.com/pedroluca', '_blank')}>
         Veja mais projetos
         <MoveRight />
       </p>
